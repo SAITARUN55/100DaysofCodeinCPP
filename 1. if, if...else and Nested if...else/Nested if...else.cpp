c0c4e92a49247a68674b948001ea04ff7fdd2e47
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-02-29 19:01:43 +0530

    Create Nested if...else.cpp

---
+++ 1. if, if...else and Nested if...else/Nested if...else.cpp
@@ -0,0 +1,25 @@
+// Program to check whether an integer is positive, negative or zero
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"Enter an integer: ";
+    cin>>number;
+    
+    if(number>0)
+    {
+        cout<<"You entered a positive integer: "<<number<<endl;
+    }
+    else if(number<0)
+    {
+        cout<<"You entered a negative integer: "<<number<<endl;
+    }
+    else
+    {
+    cout<<"This line is always printed.";
+    }
+    return 0;
+}
