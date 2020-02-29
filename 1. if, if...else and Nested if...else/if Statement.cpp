c79f3d05b4e9266cd5d5fca96b49cadc5bb43c5f
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-02-29 18:44:59 +0530

    Create if Statement.cpp

---
+++ 1. if, if...else and Nested if...else/if Statement.cpp
@@ -0,0 +1,21 @@
+// Program to print positive number entered by the user
+// If user enters negative number, it is skipped
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"Enter an integer: ";
+    cin>>number;
+    
+    // checks if the number is positive
+    if(number>0)
+    {
+        cout<<"You entered a positive integer: "<<number<<endl;
+    }
+    
+    cout<<"This statement is always executed.";
+    return 0;
+}
