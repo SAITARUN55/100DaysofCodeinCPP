ec48187136925ba11ab024860ebbf059edcc9697
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-05-05 20:26:07 +0530

    Create First Unique Character in a String.cpp

---
+++ MayLeetCodingChallenge/May 5th 2020/First Unique Character in a String.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int freq[26]={0};
+        for (char c : s)freq[c-'a']++;
+        for (int i = 0; i < s.size() ; i++) if ( freq[s[i]-'a']==1) return i;
+        return -1;
+    }
+};
