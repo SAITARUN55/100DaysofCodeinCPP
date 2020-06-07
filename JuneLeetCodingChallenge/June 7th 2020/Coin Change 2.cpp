60fa5114995651a6d466f03eac49e9c41d76ba02
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-06-07 22:20:12 +0530

    Create Coin Change 2.cpp

---
+++ JuneLeetCodingChallenge/June 7th 2020/Coin Change 2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1, 0);
+	    dp[0] = 1;
+	    for(int c:coins){
+		for(int j = c; j <= amount; ++j)
+			dp[j] += dp[j - c];
+	}
+	return dp[amount];
+    }
+};
