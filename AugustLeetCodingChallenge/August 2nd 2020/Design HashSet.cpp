26637f0ed2f50186a851dcab8953bb1f56289949
Sai Tarun Kaniganti <saitarunkaniganti@gmail.com>
2020-08-03 09:27:03 +0530

    Create Design HashSet.cpp

---
+++ AugustLeetCodingChallenge/August 2nd 2020/Design HashSet.cpp
@@ -0,0 +1,25 @@
+class MyHashSet {
+public:
+    bool arr[1000000] {false};
+    MyHashSet() {
+    }
+    
+    void add(int key) {
+        arr[key] = true;
+    }
+    
+    void remove(int key) {
+        arr[key] = false;
+    }
+    bool contains(int key) {
+        return arr[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
