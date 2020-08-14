405c9ad8056c44ea2196606eef936823ab23121b
Sai Tarun Kaniganti <saitarunkaniganti@gmail.com>
2020-08-14 23:28:41 +0530

    Create Longest Palindrome.cpp

---
+++ AugustLeetCodingChallenge/August 14th 2020/Longest Palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int>v(255,0);
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            v[s[i]-'A']++;
+            if(v[s[i]-'A']%2==0)
+            ans+=2;
+        }
+        return s.size()>ans?ans+1:ans;
+    }
+};
